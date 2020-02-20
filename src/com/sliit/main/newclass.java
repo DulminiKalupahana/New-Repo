70771122a5618e078461ac1d2de3e20d19d94463
DulminiKalupahana <dulhesh7@gmail.com>
2020-02-19 19:43:07 -0800

    new commits 1

--- src/com/sliit/main/newclass.java
+++ src/com/sliit/main/newclass.java
@@ -1,10 +1,10 @@
 package com.sliit.main;
-public class Main {
 
-	//main....
+public class newclass {
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("This is my first project at SLIIT");
+
 	}
 
 }
